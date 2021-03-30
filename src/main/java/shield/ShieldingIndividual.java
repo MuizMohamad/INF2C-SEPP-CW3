1c7b62e2cef2be8677a1a8fe1c9512ba367696a8
MuizMohamad <muizzuddinmohamad@gmail.com>
2021-03-30 12:36:40 +0100

    Updated : The new cw3 updates

--- src/main/java/shield/ShieldingIndividual.java
+++ src/main/java/shield/ShieldingIndividual.java
@@ -5,6 +5,10 @@
 package shield;
 
 public class ShieldingIndividual {
+  private String fullName;
+  private String phoneNumber;
+  private String CHINumber;
+  private boolean orderAvailibility;
   public String getCHI() {
     return null;
   }
--- src/main/java/shield/Supermarket.java
+++ src/main/java/shield/Supermarket.java
@@ -5,4 +5,7 @@
 package shield;
 
 public class Supermarket {
+    private String businessName;
+    private String phoneNumber;
+    private String websiteURl;
 }
