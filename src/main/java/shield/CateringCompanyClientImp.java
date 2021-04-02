1567f0cd30906d271f10e6c295276901ce296de1
unknown <najmideraman2000@gmail.com>
2021-04-02 04:00:13 +0100

    Test2. Implement some catering company method

--- src/main/java/shield/CateringCompanyClientImp.java
+++ src/main/java/shield/CateringCompanyClientImp.java
@@ -4,32 +4,72 @@
 
 package shield;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+
 public class CateringCompanyClientImp implements CateringCompanyClient {
+  private String endpoint;
+  private String name;
+  private String postCode;
+  private boolean isRegistered;
+
   public CateringCompanyClientImp(String endpoint) {
+    this.endpoint = endpoint;
+    this.isRegistered = false;
   }
 
   @Override
   public boolean registerCateringCompany(String name, String postCode) {
-    return false;
+    // construct the endpoint request
+    String request = " /registerCateringCompany?business_name=" + name + "&postcode=" + postCode + "'";
+
+    // setup the response recepient
+
+    String responseRegister = new String();
+
+    try {
+      // perform request
+      String response = ClientIO.doGETRequest(endpoint + request);
+
+      // unmarshal response
+      responseRegister = new Gson().fromJson(response, String.class);
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    this.isRegistered = true;
+    this.name = name;
+    this.postCode = postCode;
+    return true;
   }
 
   @Override
   public boolean updateOrderStatus(int orderNumber, String status) {
+    // construct the endpoint request
+    String request = " /updateOrderStatus?order_id=" + orderNumber + "&newStatus=" + status + "'";
     return false;
   }
 
   @Override
   public boolean isRegistered() {
-    return false;
+    return this.isRegistered;
   }
 
   @Override
   public String getName() {
-    return null;
+    return this.name;
   }
 
   @Override
   public String getPostCode() {
-    return null;
+    return this.postCode;
   }
 }
